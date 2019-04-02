ac9d102fa992411ab37ad7fbf5561ffe5f6f571f
rodrigoandrade <rodrigo.oliveira.and@gmail.com>
2019-04-02 00:40:09 -0300

    initial project back end

---
+++ trecco-web-back/src/main/java/br/com/treccowebback/TreccoWebBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.treccowebback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreccoWebBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreccoWebBackApplication.class, args);
+	}
+
+}
